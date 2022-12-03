91f590f1e5ec7b4b4444a12300bec1768315aa63
Surabhi951 <surabhibhagat95@gmail.com>
2022-12-03 22:58:55 +0530

    Modified: I am in any mood message should returns HAPPY

--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -5,19 +5,19 @@ import org.junit.jupiter.api.Test;
 
 public class MoodAnalyserTest {
 
-    @Test
+/*    @Test
     public void givenMessage_WhenProper_ShouldReturnSad(){
         MoodAnalyser moodAnalyser = new MoodAnalyser();
         String mood = moodAnalyser.analyseMood("This is a sad message");
         System.out.println(mood);
         Assertions.assertEquals("SAD", mood);
-    }
+    }*/
 
- /*   @Test
+    @Test
     public void givenMessage_WhenProper_ShouldReturnHappy(){
         MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("This is a happy message");
+        String mood = moodAnalyser.analyseMood("I am in any mood");
         System.out.println(mood);
         Assertions.assertEquals("HAPPY", mood);
-    }*/
+    }
 }
